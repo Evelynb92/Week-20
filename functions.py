ea3746b6c33a9a96b03a73fbbc58f5cbd9289f4a
ebrooks <evelynb92@hotmail.com>
2022-07-15 14:30:28 -0400

    Final (hopefully!!!!) push

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 def conversion(temperature_entry, degree_selection):
     if degree_selection == "Celsius":
         celsius = float((float(temperature_entry) * 9/5) + 32)
-        print('cel')
+        #print('cel')
         return celsius
     elif degree_selection == "Fahrenheit":
         fahrenheit = float((float(temperature_entry) - 32) * 5 / 9)
         return fahrenheit
-        print('fah')
+        #print('fah')
 
--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from functions import conversion
 
 def gui():
+
     #Window Box -----------------------
     window = tk.Tk()
     window.title('Temperature Conversion')
@@ -12,26 +13,35 @@ def gui():
     window_bg = '#FFC18E'
     button_bg = '#FFC18E'
 
-    window.config(bg= '#CA4E79')
+    window.config(bg='#CA4E79')
 
     #Widget -----------------------
     degree = tk.StringVar()
 
     def call_conversion():
-        temperature = temperature_entry.get()
-        deg = degree.get()
-        result_entry.delete(0, tk.END)
-        result_entry.insert(0, conversion(temperature, deg))
+        temp = temperature_entry.get()
 
-        if deg == "Celsius":
-            result_label.config(text='Fahrenheit')
-        elif deg == "Fahrenheit":
-            result_label.config(text='Celsius')
+        if temp.isnumeric():
+
+            deg = degree.get()
+            result_entry.delete(0,tk.END)
+            result_entry.insert(0,conversion(temp, deg))
+
+            if deg == "Celsius":
+                    result_label.config(text='<--- Fahrenheit')
+
+            elif deg == "Fahrenheit":
+                    result_label.config(text='<--- Celsius')
+
+        else:
+            result_entry.insert(tk.END,"Invaild Characters")
+
+
 
 
     #Buttons -----------------------
     convert_button = tk.Button(text="Convert", bg=button_bg)
-    convert_button.grid(row=20, column=1)
+    convert_button.grid(row=20, column=2)
     convert_button.configure(command=call_conversion)
 
     def clear_button():
@@ -40,26 +50,31 @@ def gui():
         return None
 
     clear_button = tk.Button(text="Clear", bg=button_bg, command=clear_button)
-    clear_button.grid(row=50, column=1)
+    clear_button.grid(row=100, column=2)
 
     #Dropdown Box -----------------------
     options = ["Celsius", "Fahrenheit"]
 
     drop_down = tk.OptionMenu(window, degree, *options)
-    drop_down.grid(row=0, column=2)
+    drop_down.config(bg=window_bg)
+    drop_down["menu"].config(bg=window_bg)
+    drop_down.grid(row=0, column=3)
 
     #Text Label -----------------------
     temperature_label = tk.Label(text="Enter Temperature", bg=window_bg)
     temperature_label.grid(row=0, column=0)
 
+    output_label = tk.Label(text="Results", bg=window_bg)
+    output_label.grid(row=1, column=0)
+
     result_label = tk.Label(text="",bg=window_bg)
-    result_label.grid(row=1, column=2)
+    result_label.grid(row=1, column=3)
 
     #User entry -----------------------
     temperature_entry = tk.Entry()
-    temperature_entry.grid(row=0, column=1)
+    temperature_entry.grid(row=0, column=2)
 
-    result_entry = tk.Entry()
-    result_entry.grid(row=1, column=1)
+    result_entry = tk.Entry(text="")
+    result_entry.grid(row=1, column=2)
 
     window.mainloop()
